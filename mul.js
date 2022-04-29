648a2cb677b5a3e11e5f694cf85b74ed40877ad1
Anton Franzen <anton@exploreistic.com>
2022-04-29 23:01:02 +0200

    new

--- mul.js
+++ mul.js
@@ -1,7 +1,3 @@
 const mul = () => {
-<<<<<<< HEAD
-    console.log("MulTIPLY CHANGES!!!")
-=======
-    console.log("MulNEEEW")
->>>>>>> c1732e12b45a9273b98360a4e407c0fba214689e
+    console.log("NEW AGAIN")
 }
